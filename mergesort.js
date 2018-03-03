2ca4408abdf5275c62816cb935776a1f39536b8b
andventuree <awwong92@gmail.com>
2018-03-02 23:28:30 -0500

    updated solution

--- mergesort.js
+++ mergesort.js
@@ -1,40 +1,48 @@
-function mergeSort(array) { //sorting function
-  // indicate that you need sorting???
-
-  // while(array[0].length !== 1 || array[1].length !== 1){
-  //   let first = split(array[0]);
-  //   let second = split(array[1]);
-  //   mergeSort([first, second]);
-  // }
+function mergeSort(array) { //brings entire operations together
+  //base case
+  if(array.length < 2) return array; //stop when array is just a single elem
+  let splits = split(array),
+      left = splits[0],
+      right = splits[1];
+  console.log(splits)
+  return merge(mergeSort(left), mergeSort(right))//merge whatever is sorted
 }
 
 function split(wholeArray) {
-  if(Array.isArray(wholeArray[0])){
-    split(merge(wholeArray[0], wholeArray[1]));
+  let dividingIndex =  wholeArray.length / 2; //dont need Math.floor b/c slice rounds up
+  let left = wholeArray.slice(0, dividingIndex);
+  let right = wholeArray.slice(dividingIndex);
+  // console.log(left);
+  // console.log(right);
+  return [left, right]; // [ [ ... ], [ ... ] ]
+}
+
+function merge(left, right){ //call this function to put separate elements together
+  let merged = [],
+      leftIdx = 0,
+      rightIdx = 0;
+  let counter = 0;
+  //using shift for this function makes it a lot slower! big O multiplied by n! because you need to move all the elements over by 1 to expand the array.
+  //So make use of an left and right index instead
+  while(leftIdx < left.length && rightIdx < right.length){
+    if(left[leftIdx] < right[rightIdx]){
+      merged.push(left[leftIdx])
+      leftIdx++;
+    } else {
+      merged.push(right[rightIdx])
+      rightIdx++;
+    }
+    // console.log()
+    console.log(`${counter++} passes - left [${left}], right [${right}]`)
+
   }
-  let dividingIndex = Math.floor(wholeArray.length / 2);
-  let firstHalf = wholeArray.slice(0, dividingIndex);
-  let secondHalf = wholeArray.slice(dividingIndex);
-  // console.log(firstHalf);
-  // console.log(secondHalf);
-  return [firstHalf, secondHalf]; // [ [ ... ], [ ... ] ]
+  console.log(merged);
+  for(; leftIdx < left.length; leftIdx++){ merged.push(left[leftIdx])};
+  for(; rightIdx < right.length; rightIdx++){ merged.push(right[rightIdx])};
+  return merged;
 }
 
-function merge(arr1, arr2){ //call this function to put separate elements together
-  console.log('time to merge:', arr1, ' + ', arr2);
-  return arr1.concat(arr2);
-}
+let testArr = [5,8,2,1,9,10,3,6,4,7]; //[ [5,8,2,1,9,], [10,3,6,4,7] ]
 
-let testArr = [5,8,2,1,9,10,3,6,4,7];
-console.log(mergeSort())
-
-let firstSplit = split(testArr);      //5 + 5
-// let secondSplit = split(firstSplit);  //3 + 2 + 3 + 2
-// let thirdSplit = split(secondSplit);  //2 +1 +1 +1 +2 +1 +1 +1
-
-console.log(split(testArr));
-// console.log(split(firstSplit));
-// console.log(split(secondSplit));
-// console.log(split(thirdSplit));
-
+//Ex. [1, 5, 7, 9] [2, 6, 8]
 
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,11 +1,33 @@
-describe('Split Array function', function() {
-  it('is able to split an array into two halves', function() {
-    // your code here
-  });
-});
+describe('Merge sort ', function(){
+  describe('Split Array function', function() {
+    it('given one array, return two arrays', function() {
+      expect(split([])).toEqual([[],[]]);
+    });
 
-describe('Merge function', function(){
-  it('is able to merge two sorted arrays into one sorted array', function(){
-    // test the merging algorithm
+    it('split array of even length', function(){
+      expect(split([5,10])).toEqual([[5],[10]]);
+    })
+
+    it('split array of odd length', function(){
+      expect(split([5,10,4])).toEqual([[5],[10,4]])
+    })
+
   });
-});
+
+  describe('Merge function', function(){
+    it('is able to merge two sorted arrays into one sorted array', function(){
+      expect(merge([],[])).toEqual([]);
+    });
+
+    it('given two already sorted arrays of equal length, will merge',function(){
+      expect(merge([1,2,3],[4,5,6])).toEqual([1,2,3,4,5,6])
+    });
+
+  });
+
+})
+
+
+
+
+
